c5c5277e81c7da7b9fb9556d80b2dad28e2d5654
Avanesyan Arkadi <aavanesy@outlook.com>
2020-01-27 12:21:12 +0400

    lib load upd

--- scripts/supporting/load_libraries.R
+++ scripts/supporting/load_libraries.R
@@ -1,18 +1,19 @@
 # Load packages for project, installing if necessary
 
-load_package <- function(x) {
-  for (i in x) {
-    #  require returns TRUE invisibly if it was able to load package
-    if (!require(i , character.only = TRUE)) {
-      #  If package was not able to be loaded then re-install
-      install.packages(i , dependencies = TRUE)
-      #  Load package after installing
-      require(i , character.only = TRUE)
-    }
-  }
-}
+# load_package <- function(x) {
+#   for (i in x) {
+#     #  require returns TRUE invisibly if it was able to load package
+#     if (!require(i , character.only = TRUE)) {
+#       #  If package was not able to be loaded then re-install
+#       install.packages(i , dependencies = TRUE)
+#       #  Load package after installing
+#       require(i , character.only = TRUE)
+#     }
+#   }
+# }
 
 #  Then try/install packages...
 
-packages <- c("tidyverse",'officer','readxl','rvg','mschart','extrafontdb','flextable')
-load_package(packages)
+packages <- c("dplyr",'ggplot2','purrr','officer','readxl',
+              'rvg','mschart','extrafontdb','flextable')
+lapply(packages, require, character.only = TRUE)
